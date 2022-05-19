2031384ea95d029347fd8278aa12231afda0975e
david <davidjhoyjr@gmail.com>
2022-05-19 08:30:54 -0400

    commit tree 6a65eba6510c74c06350033f5e65cfcffa1280d6

--- index.js
+++ index.js
@@ -1,1 +1,17 @@
 // Code your solutions in this file
+function writeCards(arra, occasion){
+    const newAr = [];
+    for(let i=0; i<arra.length; i++){
+        newAr.push(`Thank you, ${arra[i]}, for the wonderful ${occasion} gift!`);
+
+    }
+    return newAr;
+    
+}
+
+function countDown(num){
+    for(let i = num; i>= 0; i--){
+        console.log(i);
+    }
+}
+
